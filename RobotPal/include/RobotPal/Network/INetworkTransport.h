fffc4b5dd5b5b7f8784b52f9eaf088c36e87072f
pyo8470 <rkswlska789@naver.com>
2025-12-02 22:04:14 +0900

    네트워크 관련 코드들 작성 보류

--- RobotPal/include/RobotPal/Network/INetworkTransport.h
+++ RobotPal/include/RobotPal/Network/INetworkTransport.h
@@ -1,18 +1,18 @@
-#pragma once
-#include <string>
-#include <vector>
-#include <cstdint>
+// #pragma once
+// #include <string>
+// #include <vector>
+// #include <cstdint>
 
-class NetworkQueue;
+// class NetworkQueue;
 
-class INetworkTransport
-{
-public:
-    virtual ~INetworkTransport() = default;
+// class INetworkTransport
+// {
+// public:
+//     virtual ~INetworkTransport() = default;
 
-    virtual bool Connect(const std::string& url) = 0;
-    virtual void Disconnect() = 0;
+//     virtual bool Connect(const std::string& url) = 0;
+//     virtual void Disconnect() = 0;
 
-    virtual void Send(const std::vector<uint8_t>& data) = 0;
-    virtual bool IsConnected() const = 0;
-};
+//     virtual void Send(const std::vector<uint8_t>& data) = 0;
+//     virtual bool IsConnected() const = 0;
+// };
--- RobotPal/include/RobotPal/Network/TcpNetworkTransport.h
+++ RobotPal/include/RobotPal/Network/TcpNetworkTransport.h
@@ -1,35 +1,35 @@
-#pragma once
-#include "RobotPal/Network/INetworkTransport.h"
-#include <string>
-#include <atomic>
+// #pragma once
+// #include "RobotPal/Network/INetworkTransport.h"
+// #include <string>
+// #include <atomic>
 
-#ifdef _WIN32
-    #include <WinSock2.h>
-    typedef SOCKET SocketHandle;
-#else
-    #include <sys/socket.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
-    typedef int SocketHandle;
-    #define INVALID_SOCKET -1
-    #define SOCKET_ERROR -1
-#endif
+// #ifdef _WIN32
+//     #include <WinSock2.h>
+//     typedef SOCKET SocketHandle;
+// #else
+//     #include <sys/socket.h>
+//     #include <arpa/inet.h>
+//     #include <unistd.h>
+//     typedef int SocketHandle;
+//     #define INVALID_SOCKET -1
+//     #define SOCKET_ERROR -1
+// #endif
 
-class TcpNetworkTransport : public INetworkTransport
-{
-public:
-    TcpNetworkTransport();
-    virtual ~TcpNetworkTransport();
+// class TcpNetworkTransport : public INetworkTransport
+// {
+// public:
+//     TcpNetworkTransport();
+//     virtual ~TcpNetworkTransport();
 
-    bool Connect(const std::string& url) override;
-    void Disconnect() override;
-    void Send(const std::vector<uint8_t>& data) override;
-    bool IsConnected() const override;
+//     bool Connect(const std::string& url) override;
+//     void Disconnect() override;
+//     void Send(const std::vector<uint8_t>& data) override;
+//     bool IsConnected() const override;
 
-    // Engine에서 RecvLoop를 돌릴 때 사용
-    int ReceiveRaw(uint8_t* buf, int maxSize);
+//     // Engine에서 RecvLoop를 돌릴 때 사용
+//     int ReceiveRaw(uint8_t* buf, int maxSize);
 
-private:
-    SocketHandle m_Socket = INVALID_SOCKET;
-    std::atomic<bool> m_IsConnected{false};
-};
+// private:
+//     SocketHandle m_Socket = INVALID_SOCKET;
+//     std::atomic<bool> m_IsConnected{false};
+// };
